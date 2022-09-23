a727939964a9e2c5ecd4f5abe225f729505870be
kowalsking <kovvalsking@gmail.com>
2022-09-23 11:25:07 +0300

    Right place of image

--- assets/canvas.js
+++ assets/canvas.js
@@ -2,4 +2,4 @@ const canvas = document.querySelector('canvas')
 const sandbox = new GlslCanvas(canvas)
 
 sandbox.load(frag)
-sandbox.setUniform('displacement', 'displacement1.jpg')
+sandbox.setUniform('displacement', '../assets/displacement1.jpg')
