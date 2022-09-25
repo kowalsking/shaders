a2513ae75b34389642928db726d8c5c47f14a27c
kowalsking <kovvalsking@gmail.com>
2022-09-25 20:23:39 +0300

    2. Kaleidoscope Fragment shader

--- assets/canvas.js
+++ assets/canvas.js
@@ -1,4 +1,20 @@
 const canvas = document.querySelector('div.canvas-holder canvas')
 const sandbox = new GlslCanvas(canvas)
 
+const calcSize = function() {
+  let ww = window.innerWidth
+  let wh = window.innerHeight
+  let dpi = window.devicePixelRatio
+
+  let s = Math.max(ww, wh)
+
+  canvas.width = s * dpi
+  canvas.height = s * dpi
+  canvas.style.width = s + 'px'
+  canvas.style.height = s + 'px'
+}
+
+calcSize()
+
 sandbox.load(frag)
+sandbox.setUniform('image', '../assets/trails.jpg')
